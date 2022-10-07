27b7016f4aa794c0c037e5f75569add22bc9b589
Jason King <jk1ng@yahoo.com>
2022-10-06 20:52:35 -0700

    Added additional required env vars

--- gitlab-project-create.sh
+++ gitlab-project-create.sh
@@ -8,5 +8,9 @@ curl --request POST -sSLk --header "PRIVATE-TOKEN:$GITLAB_TOKEN" "http://$GITLAB
 curl --request POST -sSLk --header "PRIVATE-TOKEN:$GITLAB_TOKEN" "http://$GITLAB_HOST/api/v4/projects/$GITLAB_USERNAME%2f$GITLAB_PROJECT/variables" --form "key=CML_PASSWORD" --form "value=$CML_PASSWORD" 
 curl --request POST -sSLk --header "PRIVATE-TOKEN:$GITLAB_TOKEN" "http://$GITLAB_HOST/api/v4/projects/$GITLAB_USERNAME%2f$GITLAB_PROJECT/variables" --form "key=CML_LAB" --form "value=$CML_LAB"
 curl --request POST -sSLk --header "PRIVATE-TOKEN:$GITLAB_TOKEN" "http://$GITLAB_HOST/api/v4/projects/$GITLAB_USERNAME%2f$GITLAB_PROJECT/variables" --form "key=CML_VERIFY_CERT" --form "value=false"
+curl --request POST -sSLk --header "PRIVATE-TOKEN:$GITLAB_TOKEN" "http://$GITLAB_HOST/api/v4/projects/$GITLAB_USERNAME%2f$GITLAB_PROJECT/variables" --form "key=NETBOX_API" --form "value=$NETBOX_API"
+curl --request POST -sSLk --header "PRIVATE-TOKEN:$GITLAB_TOKEN" "http://$GITLAB_HOST/api/v4/projects/$GITLAB_USERNAME%2f$GITLAB_PROJECT/variables" --form "key=NETBOX_TOKEN" --form "value=$NETBOX_TOKEN"
+curl --request POST -sSLk --header "PRIVATE-TOKEN:$GITLAB_TOKEN" "http://$GITLAB_HOST/api/v4/projects/$GITLAB_USERNAME%2f$GITLAB_PROJECT/variables" --form "key=NSO_USERNAME" --form "value=$NSO_USERNAME"
+curl --request POST -sSLk --header "PRIVATE-TOKEN:$GITLAB_TOKEN" "http://$GITLAB_HOST/api/v4/projects/$GITLAB_USERNAME%2f$GITLAB_PROJECT/variables" --form "key=NSO_PASSWORD" --form "value=$NSO_PASSWORD"
 # Push repo into project
 git push http://$GITLAB_USERNAME:$GITLAB_PASSWORD@$GITLAB_HOST/$GITLAB_USERNAME/$GITLAB_PROJECT
